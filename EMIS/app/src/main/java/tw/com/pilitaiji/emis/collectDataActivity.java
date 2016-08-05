296f3fd1811d561582059f149a7f3f7256b5118e
pilitaiji <jny861108@gmail.com>
2016-08-06 00:19:17 +0800

    add more 3 save buttons

--- EMIS/app/src/main/java/tw/com/pilitaiji/emis/collectDataActivity.java
+++ EMIS/app/src/main/java/tw/com/pilitaiji/emis/collectDataActivity.java
@@ -40,34 +40,134 @@ public class collectDataActivity extends AppCompatActivity {
         hurt_name = (EditText)findViewById(R.id.hurt_name);
         police_number = (EditText)findViewById(R.id.police_number);
 
-        FileInputStream fis = null;
-        StringBuilder sb = new StringBuilder();
+//package_name
+        FileInputStream fis_package_name = null;
+        StringBuilder sb_package_name = new StringBuilder();
         try {
-            fis = openFileInput("note.txt");
-            InputStreamReader isr = new InputStreamReader(fis);
-            BufferedReader br = new BufferedReader(isr);
+            fis_package_name = openFileInput("package_name.txt");
+            InputStreamReader isr_package_name = new InputStreamReader(fis_package_name);
+            BufferedReader br_package_name = new BufferedReader(isr_package_name);
             String str = "";
-            while ((str = br.readLine()) != null) {
-                sb.append(str);
+            while ((str = br_package_name.readLine()) != null) {
+                sb_package_name.append(str);
             }
-            br.close();
-            isr.close();
-            fis.close();
+            br_package_name.close();
+            isr_package_name.close();
+            fis_package_name.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        package_name.setText(sb);
+        package_name.setText(sb_package_name);
+
+//case_name
+        FileInputStream fis_case_name = null;
+        StringBuilder sb_case_name = new StringBuilder();
+        try {
+            fis_case_name = openFileInput("case_name.txt");
+            InputStreamReader isr_case_name = new InputStreamReader(fis_case_name);
+            BufferedReader br_case_name = new BufferedReader(isr_case_name);
+            String str = "";
+            while ((str = br_case_name.readLine()) != null) {
+                sb_case_name.append(str);
+            }
+            br_case_name.close();
+            isr_case_name.close();
+            fis_case_name.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        case_name.setText(sb_case_name);
+
+//time_name
+        FileInputStream fis_time_name = null;
+        StringBuilder sb_time_name = new StringBuilder();
+        try {
+            fis_time_name = openFileInput("time_name.txt");
+            InputStreamReader isr_time_name = new InputStreamReader(fis_time_name);
+            BufferedReader br_time_name = new BufferedReader(isr_time_name);
+            String str = "";
+            while ((str = br_time_name.readLine()) != null) {
+                sb_time_name.append(str);
+            }
+            br_time_name.close();
+            isr_time_name.close();
+            fis_time_name.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        time_name.setText(sb_time_name);
+
+//location_name
+        FileInputStream fis_location_name = null;
+        StringBuilder sb_location_name = new StringBuilder();
+        try {
+            fis_location_name = openFileInput("location_name.txt");
+            InputStreamReader isr_location_name = new InputStreamReader(fis_location_name);
+            BufferedReader br_location_name = new BufferedReader(isr_location_name);
+            String str = "";
+            while ((str = br_location_name.readLine()) != null) {
+                sb_location_name.append(str);
+            }
+            br_location_name.close();
+            isr_location_name.close();
+            fis_location_name.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        location_name.setText(sb_location_name);
     }
 
     public void save(View view) {
-        FileOutputStream fos = null;
-
+//package_name
+        FileOutputStream fos_package_name = null;
         try {
-            fos = openFileOutput("note.txt", Context.MODE_PRIVATE);
-            fos.write(package_name.getText().toString().getBytes());
-            fos.close();
+            fos_package_name = openFileOutput("package_name.txt", Context.MODE_PRIVATE);
+            fos_package_name.write(package_name.getText().toString().getBytes());
+            fos_package_name.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//case_name
+        FileOutputStream fos_case_name = null;
+        try {
+            fos_case_name = openFileOutput("case_name.txt", Context.MODE_PRIVATE);
+            fos_case_name.write(case_name.getText().toString().getBytes());
+            fos_case_name.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//time_name
+        FileOutputStream fos_time_name = null;
+        try {
+            fos_time_name = openFileOutput("time_name.txt", Context.MODE_PRIVATE);
+            fos_time_name.write(time_name.getText().toString().getBytes());
+            fos_time_name.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//location_name
+        FileOutputStream fos_location_name = null;
+        try {
+            fos_location_name = openFileOutput("location_name.txt", Context.MODE_PRIVATE);
+            fos_location_name.write(location_name.getText().toString().getBytes());
+            fos_location_name.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
